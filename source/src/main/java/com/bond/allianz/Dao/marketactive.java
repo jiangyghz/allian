bccc2551e78d24343197fd806537ab9dcf3c4ebb
Dwain Derion <92196723+jiangyghz@users.noreply.github.com>
2024-09-26 10:18:26 +0800

    Delete source/src directory

--- source/src/main/java/com/bond/allianz/Dao/marketactive.java
+++
@@ -1,8 +0,0 @@
-package com.bond.allianz.Dao;
-
-public class marketactive {
-    public  float amount;
-    public  int atype;
-    public  boolean valid=false;
-    public  String productid="";
-}
